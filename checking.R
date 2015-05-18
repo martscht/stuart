0be18bef2198088ac2e93c0645e24e51236b488a
Martin Schultze <martin.schultze@fu-berlin.de>
2015-05-18 14:19:07 +0200

    Add new checks

---
+++ checking.R
@@ -0,0 +1,14 @@
+library(stuart)
+
+data(fairplayer)
+names(fairplayer)
+
+fs <- list(SI1=names(fairplayer)[2:11],
+  SI2=names(fairplayer[12:21]))
+
+repme <- list(SI=c('SI1','SI2'))
+
+mmas_mplus <- mmas(fairplayer, fs, 1, 3, repeated.measures=repme, pbest=.1, software='Mplus')
+mmas_lavaan <- mmas(fairplayer, fs, 1, 3,  repeated.measures=repme, pbest=.1, software='lavaan')
+bf_mplus <- bruteforce(fairplayer, fs, 1, 3, repeated.measures=repme, software='Mplus')
+bf_lavaan <- bruteforce(fairplayer, fs, 1, 3, repeated.measures=repme, software='lavaan')
