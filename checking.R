d224a2ce0ae8dd474c986e20f2eaa231a918a482
Martin <martin.schultze@fu-berlin.de>
2015-06-30 13:39:53 +0200

    Add documentation for crossvalidate()

--- checking.R
+++ checking.R
@@ -12,3 +12,6 @@ mmas_mplus <- mmas(fairplayer, fs, 1, 3, repeated.measures=repme, pbest=.1, soft
 mmas_lavaan <- mmas(fairplayer, fs, 1, 3,  repeated.measures=repme, pbest=.1, software='lavaan')
 bf_mplus <- bruteforce(fairplayer, fs, 1, 3, repeated.measures=repme, software='Mplus')
 bf_lavaan <- bruteforce(fairplayer, fs, 1, 3, repeated.measures=repme, software='lavaan')
+
+crossvalidate(mmas_mplus,fairplayer[1:45,],fairplayer,invariance='strict')
+crossvalidate(bf_mplus,fairplayer[1:45,],fairplayer,invariance='strict')
