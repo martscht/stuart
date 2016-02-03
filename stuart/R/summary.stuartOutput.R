b2653de0d9ff6eb065aa5b2c1b919cf2c896490f
Martin Schultze <martin.schultze@fu-berlin.de>
2016-02-03 13:53:52 +0100

    Align summary arguments with general method arguments

--- stuart/R/summary.stuartOutput.R
+++ stuart/R/summary.stuartOutput.R
@@ -1,15 +1,15 @@
 #' @export
 
 summary.stuartOutput <-
-function(x,...) {
-  Time <- as.numeric(x$timer[3])
-  Models <- nrow(x$log)
-  Replications <- sum(x$log$pheromone==max(x$log$pheromone))
-  Results <- x$log[x$log$pheromone==cummax(x$log$pheromone),]
+function(object,...) {
+  Time <- as.numeric(object$timer[3])
+  Models <- nrow(object$log)
+  Replications <- sum(object$log$pheromone==max(object$log$pheromone))
+  Results <- object$log[object$log$pheromone==cummax(object$log$pheromone),]
   Results <- Results[!duplicated(Results[,3:ncol(Results)]),]
 
-  Out <- list(Subtests=x$subtests,Results=Results,Time=Time,Models=Models,
-              Replications=Replications,Type=paste(x$call),Software=x$software)
+  Out <- list(Subtests=object$subtests,Results=Results,Time=Time,Models=Models,
+              Replications=Replications,Type=paste(object$call),Software=object$software)
   class(Out) <- 'summary.stuartOutput'
   return(Out)
 }
