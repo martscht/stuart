1269be12bfe9016ddfb337db030b5b08e36829d7
Martin Schultze <martin.schultze@fu-berlin.de>
2016-02-10 11:07:09 +0100

    Replace fairplayer with fairplayer_mtmm

--- startup_new.R
+++ startup_new.R
@@ -5,7 +5,6 @@ setwd('~/stuart')
 load_all('./stuart')
 
 load('./stuart/data/fairplayer.rda')
-load('./stuart/data/fairplayer_mtmm.rda')
 load('./stuart/data/sups.rda')
 
 # Generate documentation after changes in files
