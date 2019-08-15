9be819e85dee5c514aad2be00a7c5f9b3b886692
Martin Schultze <schultze@psych.uni-frankfurt.de>
2019-08-15 12:18:59 +0200

    Fixed bug leading to better pheromone for lower reliability.

--- stuart/R/objective.preset.R
+++ stuart/R/objective.preset.R
@@ -1,5 +1,5 @@
 objective.preset <- function(chisq, df, pvalue, rmsea, srmr, crel){
-    1 / (1 + exp(10*(crel-.6))) +
+    1 / (1 + exp(-10*(crel-.6))) +
     .5 * (1 - (1 / (1 + exp(-100*(rmsea-.05))))) +
     .5 * (1 - (1 / (1 +  exp(-100*(srmr-.06)))))
 }
@@ -7,10 +7,10 @@ objective.preset <- function(chisq, df, pvalue, rmsea, srmr, crel){
 objective.preset.comparisons <- function(chisq, df, pvalue, rmsea, srmr, cfi, crel,
   delta.chisq, delta.df, delta.pvalue, delta.rmsea, delta.srmr, delta.cfi, delta.crel) {
   
-  1 / (1 + exp(10*(crel-.6))) +
+  1 / (1 + exp(-10*(crel-.6))) +
   .5 * (1 - (1 / (1 + exp(-100*(rmsea-.05))))) +
   .5 * (1 - (1 / (1 +  exp(-100*(srmr-.06))))) +
-  1 / (1 + exp(30*(delta.crel-.1))) +
+  1 / (1 + exp(-30*(delta.crel-.1))) +
   .5 * (1 - (1 / (1 + exp(-300*(delta.rmsea-.01))))) +
   .5 * (1 - (1 / (1 + exp(-300*(delta.srmr-.01)))))    
 }
