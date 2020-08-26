70fccc53b001709e4338af6649a6667a826c30c9
Martin Schultze <schultze@psych.uni-frankfurt.de>
2020-08-26 13:15:38 +0200

    Finish initial kfold functionalities. Add documentation.

--- stuart/R/kfold.R
+++ stuart/R/kfold.R
@@ -1,3 +1,62 @@
+### Roxygen-izable Documentation ----
+#' k-Folds Crossvalidation
+#' 
+#' k-Folds crossvalidation for item selection using any approach implemented in STUART.
+#' 
+#' @details The function splits the provided data into k subsets using \code{\link{holdout}} and runs the item-selection procedure requested via \code{type} on the training datasets. Validation is performed using \code{\link{crossvalidate}} to check for invariance of the measurement models between the training and validation data up to the invariance level provided via \code{max.invariance}. The final item selection is based on the highest value on the objective function in the multiple-group SEM imposing \code{max.invariance} between the training and validation data.
+#' 
+#' @author Martin Schultze
+#' 
+#' @seealso \code{\link{holdout}} \code{\link{crossvalidate}}
+#' 
+#' @param type A \code{character} calling the item-selection procedure. Can be one of "mmas" (see \code{\link{mmas}}), "gene" (see \code{\link{gene}}), "bruteforce" (see \code{\link{bruteforce}}), or randomsamples (see \code{\link{randomsamples}}).
+#' @param k The number of folds.
+#' @param max.invariance The maximum measurement invariance level which will be tested. Currently there are four options: 'configural', 'weak', 'strong', and 'strict' (the default). All levels below \code{max.invariance} are also tested. 
+#' @param ... Arguments passed to the item-selection procedure called with \code{type}.
+#' @param remove.details A \code{logical} indicating whether to remove detailed information such as models and copies of datasets. Reduces output size by approx. 90\%. Defaults to \code{TRUE}.
+#' 
+#' @return Returns an object of the class \code{stuartKfold} for which specific \code{summary} and \code{print} methods are available. The results are a list.
+#' \item{call }{The called function.}
+#' \item{subtests}{A list containing the names of the selected items and their respective subtests.}
+#' \item{solution}{A list of matrices with the choices made in the global-best solution.}
+#' \item{final}{The results of the estimation of the solution leading to best objective value when cross-validated.}
+#' \item{frequencies}{A list of matrices showing the relative frequencies with which an item was selected across the k-folds.}
+#' \item{full}{A list of the results returned by the k runs of \code{type}.}
+#' \item{crossvalidations}{A list of data.frames showing the fit and model comparisons of all invariance levels up to \code{max.invariance} in each of the k folds.}
+#' \item{data}{A \code{data.frame}. The same as the original \code{data.frame} provided to \code{data} with the added variable \code{stuartKfold} indicating which fold an observation was assigned to.}
+#' 
+#' @examples
+#' 
+#' # k-Folding for a simple bruteforce selection
+#' data(fairplayer)
+#' fs <- list(ra = names(fairplayer)[53:57])
+#' 
+#' sel <- kfold('bruteforce', k = 5,
+#'   data = fairplayer, factor.structure = fs, 
+#'   capacity = 3, seed = 55635,
+#'   cores = 1)
+#' summary(sel)
+#' 
+#' \donttest{
+#' ### longitudinal example with mmas ----
+#' data(fairplayer)
+#' fs <- list(si1 = names(fairplayer)[83:92],
+#'   si2 = names(fairplayer)[93:102],
+#'   si3 = names(fairplayer)[103:112])
+#' 
+#' repe <- list(si = c('si1', 'si2', 'si3'))
+#' 
+#' sel_mmas <- kfold('mmas', k = 5,
+#'   data = fairplayer, factor.structure = fs,
+#'   repeated.measures = repe, long.invariance = 'strong',
+#'   capacity = 3, seed = 55635, pbest = .5)
+#' summary(sel_mmas)
+#' }
+#' 
+#' 
+#' @export
+
+
 kfold <- function(type, k = 5,
   max.invariance = 'strict',
   ...,
@@ -26,11 +85,13 @@ kfold <- function(type, k = 5,
   
   # Run searches
   searches <- list()
+
+  run_args <- args[names(args) %in% names(formals(type))]
   
   for (i in 1:k) {
-    args$data <- folded[[i]]
+    run_args$data <- folded[[i]]
     message(paste0('\nRunning fold number ', i, ' of ', k, '.\n'))
-    searches[[i]] <- try(do.call(type, args))
+    searches[[i]] <- try(do.call(type, run_args))
   }
   
   check <- sapply(searches, function(x) 'try-error' %in% class(x))
@@ -47,9 +108,10 @@ kfold <- function(type, k = 5,
   }
   
   # Reorganize solutions
-  solu <- searches[[1]][['solution']]
+  tmp <- sapply(searches, function(x) 'try-error' %in% class(x))
+  solu <- searches[[which.min(tmp)]][['solution']]
   for (i in seq_along(solu)) {
-    solu[[i]] <- do.call('rbind', lapply(lapply(searches, `[[`, 'solution'), `[[`, i))
+    solu[[i]] <- do.call('rbind', lapply(lapply(searches[!tmp], `[[`, 'solution'), `[[`, i))
   }
   
   # identify best solution
